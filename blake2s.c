853526b0f817c1a6787e9aa394ca669eeaa1e642
Thomas Pornin <thomas.pornin@nccgroup.com>
2022-09-24 16:36:01 -0400

    Fixed dec32le/enc32le in BLAKE2s for non-little-endian systems.

--- blake2s.c
+++ blake2s.c
@@ -140,10 +140,10 @@ dec32le(const void *src)
 {
 	const uint8_t *buf = src;
 
-	return (uint32_t)src[0]
-		| ((uint32_t)src[1] << 8)
-		| ((uint32_t)src[2] << 16)
-		| ((uint32_t)src[3] << 24);
+	return (uint32_t)buf[0]
+		| ((uint32_t)buf[1] << 8)
+		| ((uint32_t)buf[2] << 16)
+		| ((uint32_t)buf[3] << 24);
 }
 
 static inline void
@@ -151,10 +151,10 @@ enc32le(void *dst, uint32_t x)
 {
 	uint8_t *buf = dst;
 
-	dst[0] = (uint8_t)x;
-	dst[1] = (uint8_t)(x >> 8);
-	dst[2] = (uint8_t)(x >> 16);
-	dst[3] = (uint8_t)(x >> 24);
+	buf[0] = (uint8_t)x;
+	buf[1] = (uint8_t)(x >> 8);
+	buf[2] = (uint8_t)(x >> 16);
+	buf[3] = (uint8_t)(x >> 24);
 }
 
 #endif
